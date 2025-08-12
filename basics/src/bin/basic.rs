b5569f1069e5e77649a1a90e425448d41eb2588e
binayak9932 <mondalbinayak9932@gmail.com>
2025-08-12 21:05:53 +0530

    new

--- basics/src/bin/basic.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let x = 5; // immutable
-    let mut y = 10; // mutable
-    println!("x = {}, y = {}", x, y);
-
-    y = y + 5;
-    println!("New y = {}", y);
-}
---
+++ rust_dsa/bin/var_mut.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let x = 5; // x is immutable.
+    println!("The value of x is: {}", x);
+
+    // The line below would cause a compile error!
+    // x = 6; 
+}
