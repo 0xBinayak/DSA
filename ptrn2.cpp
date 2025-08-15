9dd70684146c28147e0a01528b4a04eb042ee5f5
Binayak <mondalbinayak9932@gmail.com>
2025-08-15 21:44:30 +0530

    mac

---
+++ ptrn2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int row=1;
+    char ch='A';
+    while(row<=n){
+
+        int col =1;
+        while (col<=n)
+        {
+            cout<<ch;
+            col+=1;
+            ch+=1;
+
+
+        }
+        cout<<endl;
+        row+=1;
+
+    }
+    return 0;
+
+}
